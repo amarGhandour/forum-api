df9ab95745a277fda7df5855c9a9931702a87ad3
amarGhandour <amarghandour89@gmail.com>
2022-02-14 16:00:02 +0200

    [add-tests] make tests for thread store request

--- tests/Feature/Thread/CreateThreadTest.php
+++ tests/Feature/Thread/CreateThreadTest.php
@@ -53,4 +53,202 @@ class CreateThreadTest extends TestCase
 
     }
 
+    public function test_it_validates_that_the_title_is_required_when_creating_a_thread(): void
+    {
+
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+
+        $thread = Thread::factory()->make([
+            'user_id' => null
+        ]);
+
+        $resourceObject = ThreadResource::make($thread)
+            ->hide(['data.id', 'data.author', 'data.title'])
+            ->response()
+            ->getData('true');
+
+        $this->postJson('api/v1/threads', $resourceObject)
+            ->assertStatus(422)->assertJson([
+                'errors' => [
+                    [
+                        'title' => 'Validation Error',
+                        'details' => 'The data.title field is required.',
+                        'source' => [
+                            'pointer' => '/data/title'
+                        ]
+                    ]
+                ]
+            ]);
+
+        $this->assertDatabaseMissing('threads', $resourceObject['data']);
+
+    }
+
+    public function test_it_validates_that_the_title_must_be_string_when_creating_a_thread(): void
+    {
+
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+
+        $thread = Thread::factory()->make([
+            'user_id' => null,
+            'title' => 3874837,
+        ]);
+
+        $resourceObject = ThreadResource::make($thread)
+            ->hide(['data.id', 'data.author'])
+            ->response()
+            ->getData('true');
+
+        $this->postJson('api/v1/threads', $resourceObject)
+            ->assertStatus(422)->assertJson([
+                'errors' => [
+                    [
+                        'title' => 'Validation Error',
+                        'details' => 'The data.title must be a string.',
+                        'source' => [
+                            'pointer' => '/data/title'
+                        ]
+                    ]
+                ]
+            ]);
+
+        $this->assertDatabaseMissing('threads', $resourceObject['data']);
+
+    }
+
+    public function test_it_validates_that_the_body_is_required_when_creating_a_thread(): void
+    {
+
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+
+        $thread = Thread::factory()->make([
+            'user_id' => null
+        ]);
+
+        $resourceObject = ThreadResource::make($thread)
+            ->hide(['data.id', 'data.author', 'data.body'])
+            ->response()
+            ->getData('true');
+
+        $this->postJson('api/v1/threads', $resourceObject)
+            ->assertStatus(422)->assertJson([
+                'errors' => [
+                    [
+                        'title' => 'Validation Error',
+                        'details' => 'The data.body field is required.',
+                        'source' => [
+                            'pointer' => '/data/body'
+                        ]
+                    ]
+                ]
+            ]);
+
+        $this->assertDatabaseMissing('threads', $resourceObject['data']);
+
+    }
+
+    public function test_it_validates_that_the_body_must_be_string_when_creating_a_thread(): void
+    {
+
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+
+        $thread = Thread::factory()->make([
+            'user_id' => null,
+            'body' => 3874837,
+        ]);
+
+        $resourceObject = ThreadResource::make($thread)
+            ->hide(['data.id', 'data.author'])
+            ->response()
+            ->getData('true');
+
+        $this->postJson('api/v1/threads', $resourceObject)
+            ->assertStatus(422)->assertJson([
+                'errors' => [
+                    [
+                        'title' => 'Validation Error',
+                        'details' => 'The data.body must be a string.',
+                        'source' => [
+                            'pointer' => '/data/body'
+                        ]
+                    ]
+                ]
+            ]);
+
+        $this->assertDatabaseMissing('threads', $resourceObject['data']);
+
+    }
+
+    public function test_it_validates_that_the_slug_is_required_when_creating_a_thread(): void
+    {
+
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+
+        $thread = Thread::factory()->make([
+            'user_id' => null
+        ]);
+
+        $resourceObject = ThreadResource::make($thread)
+            ->hide(['data.id', 'data.author', 'data.slug'])
+            ->response()
+            ->getData('true');
+
+        $this->postJson('api/v1/threads', $resourceObject)
+            ->assertStatus(422)->assertJson([
+                'errors' => [
+                    [
+                        'title' => 'Validation Error',
+                        'details' => 'The data.slug field is required.',
+                        'source' => [
+                            'pointer' => '/data/slug'
+                        ]
+                    ]
+                ]
+            ]);
+
+        $this->assertDatabaseMissing('threads', $resourceObject['data']);
+
+    }
+
+    public function test_it_validates_that_the_slug_must_be_unique_when_creating_a_thread(): void
+    {
+
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+
+        $threadInDatabase = Thread::factory()->create();
+
+        $thread = Thread::factory()->make([
+            'user_id' => null,
+            'slug' => $threadInDatabase->slug,
+        ]);
+
+        $resourceObject = ThreadResource::make($thread)
+            ->hide(['data.id', 'data.author'])
+            ->response()
+            ->getData('true');
+
+        $this->postJson('api/v1/threads', $resourceObject)
+            ->assertStatus(422)->assertJson([
+                'errors' => [
+                    [
+                        'title' => 'Validation Error',
+                        'details' => 'The data.slug has already been taken.',
+                        'source' => [
+                            'pointer' => '/data/slug'
+                        ]
+                    ]
+                ]
+            ]);
+
+        $this->assertDatabaseMissing('threads', $resourceObject['data']);
+
+    }
+
+
 }

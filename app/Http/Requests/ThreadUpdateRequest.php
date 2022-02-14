d7e36740ad7ff1c44a13f3c5a8089e34fe778785
amarGhandour <amarghandour89@gmail.com>
2022-02-14 13:49:37 +0200

    [add-files] add store and update requests for thread.

---
+++ app/Http/Requests/ThreadUpdateRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class ThreadUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'data.title' => ['required', 'string'],
+            'data.body' => ['required', 'string'],
+            'data.slug' => ['required', Rule::unique('threads', 'slug')
+                ->ignore($this->route('thread'))],
+        ];
+    }
+}
